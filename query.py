62d1042b8bf41734d339940b0e9d7654c6269c29
Jacob LaRock <jacoblarock@gmail.com>
2025-06-15 17:51:15 +0200

    add answer streaming

--- query.py
+++ query.py
@@ -2,7 +2,7 @@ from ollama import chat, ChatResponse
 import json
 import db_cols
 
-model = "deepseek-r1:7b"
+model = "llama3.1:8b"
 
 def get_context_needed(query: str) -> list[str]:
     with open("prompts/additional_context") as file:
@@ -23,7 +23,7 @@ def get_context_needed(query: str) -> list[str]:
         message = []
     return message
 
-def answer_with_context(query: str, context: list | None):
+def answer_with_context(query: str, context: list | None, print_stream: bool = False):
     with open("prompts/answer_with_context") as file:
         prompt = file.read().replace("{user_query}", f'"{query}"').replace("{context}", str(context))
     response: ChatResponse = chat(
@@ -34,9 +34,17 @@ def answer_with_context(query: str, context: list | None):
         }],
         options={
             "num_ctx": 8192
-        }
+        },
+        stream=print_stream
     )
-    return response.message.content
+    message = ""
+    if print_stream:
+        for chunk in response:
+            message += chunk.message.content
+            print(chunk.message.content, end="", flush=True)
+    else:
+        message = response.message.content
+    return message
 
 if __name__ == "__main__":
     query = input()
@@ -46,5 +54,5 @@ if __name__ == "__main__":
     col = client.get_collection("default")
     context = db_cols.query(col, keywords)
     print(len(str(context)))
-    answer = answer_with_context(query, context)
+    answer = answer_with_context(query, context, print_stream=True)
     print(answer)
