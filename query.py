72facfb77a092ce9450016f040bf1f6f1798e5aa
Jacob LaRock <jacoblarock@gmail.com>
2025-06-15 16:06:42 +0200

    replace llama with deepseek 7b and reduce context window

--- query.py
+++ query.py
@@ -2,12 +2,14 @@ from ollama import chat, ChatResponse
 import json
 import db_cols
 
+model = "deepseek-r1:7b"
+
 def get_context_needed(query: str) -> list[str]:
     with open("prompts/additional_context") as file:
         prompt = file.read().replace("{user_query}", f'"{query}"')
     try:
         response: ChatResponse = chat(
-            model="llama3.1:8b",
+            model=model,
             messages=[{
                 "role": "user",
                 "content": prompt
@@ -25,13 +27,13 @@ def answer_with_context(query: str, context: list | None):
     with open("prompts/answer_with_context") as file:
         prompt = file.read().replace("{user_query}", f'"{query}"').replace("{context}", str(context))
     response: ChatResponse = chat(
-        model="llama3.1:8b",
+        model=model,
         messages=[{
             "role": "user",
             "content": prompt
         }],
         options={
-            "num_ctx": 32768
+            "num_ctx": 8192
         }
     )
     return response.message.content
@@ -41,9 +43,8 @@ if __name__ == "__main__":
     keywords = get_context_needed(query)
     print(keywords)
     client = db_cols.load_client("client")
-    col = client.get_collection("test")
+    col = client.get_collection("default")
     context = db_cols.query(col, keywords)
     print(len(str(context)))
     answer = answer_with_context(query, context)
     print(answer)
-
