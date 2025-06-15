22ec7039a335782d7dcfe63209e4947fcf03b743
Jacob LaRock <jacoblarock@gmail.com>
2025-06-15 16:06:19 +0200

    replace aspose with pypdf for better processing

--- db_cols.py
+++ db_cols.py
@@ -1,8 +1,8 @@
 import chromadb
 from chromadb.api import ClientAPI
 from glob import glob
-from aspose.pdf import Document
-from aspose.pdf.text import TextAbsorber
+from pypdf import PdfReader
+import argparse
 
 def load_client(path: str) -> ClientAPI:
     return chromadb.PersistentClient(path)
@@ -29,10 +29,10 @@ def add_to_collection(col: chromadb.Collection, base_dir: str, chunk_size: int):
             with open(path, "r") as file:
                 text = file.read()
         if ext == "pdf":
-            document = Document(path)
-            text_abs = TextAbsorber()
-            document.pages.accept(text_abs)
-            text = text_abs.text
+            reader = PdfReader(path)
+            text = ""
+            for page in reader.pages:
+                text += page.extract_text()
         if text:
             chunks = chunk_text(text, chunk_size)
             print("processing", path)
@@ -47,11 +47,19 @@ def add_to_collection(col: chromadb.Collection, base_dir: str, chunk_size: int):
 
 def query(col: chromadb.Collection,texts: list[str]) -> list[list[str]] | None:
     result = col.query(
-        query_texts=texts, n_results=3
+        query_texts=texts, n_results=1
     )
     return result["documents"]
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path")
+    parser.add_argument("-n", "--name")
+    args = parser.parse_args()
+    if args.name:
+        name = args.name
+    else:
+        name = "default"
     client = load_client("client")
-    col = client.get_or_create_collection("test")
-    add_to_collection(col, "test_data/", 2000)
+    col = client.get_or_create_collection("default")
+    add_to_collection(col, args.path, 2000)
